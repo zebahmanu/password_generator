bd43454f4c3ddafa46cf8164d48a73dd9d54be57
Zebah Manu <zebahmanu@gmail.com>
2024-12-16 14:21:15 +0000

    Include special characters in password options and fix error about length

--- trial.py
+++ trial.py
@@ -2,23 +2,21 @@ import string
 import random
 
 length= int(input("What is the length: "))
-
-choice= int(input(" Press 1 for letters, Press 2 for digits, Press 3 for both"))
-
+choice= int(input(" Press 1 for letters, Press 2 for digits, Press 3 for all characters: "))
 
 def password_generator(choice, length):
     letters= string.ascii_letters
-    numbers= map(str, range(0,9))
-    print(numbers)
-    lettersAndNumbers= [letters, numbers] #initialzing variable for both 
+    special_characters= string.punctuation
+    numbers=string.digits
+    lettersNumbersCharacters= letters+numbers+special_characters #initialzing variable for all 3
     password=""
-    while(len(password)<=length):
+    while(len(password)<length):
         if (choice==1):
             password+=random.choice(letters)
         elif (choice==2):
             password+=random.choice(numbers)
         elif (choice==3):
-            password+=random.choice(lettersAndNumbers)
+            password+=random.choice(lettersNumbersCharacters)
         else:
             choice= input("Enter a valid option")
     print("Your password: "+password)
