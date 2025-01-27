6eac73e42d100cffcdb7527f44014ea812b33048
Zebah Manu <zebahmanu@gmail.com>
2025-01-27 09:36:15 +0000

    Rearrange and optimise code

--- password_generator.py
+++ password_generator.py
@@ -1,28 +1,36 @@
 import string
 import random
 
-length= int(input("What is the length: "))
-choice= int(input(" Press 1 for letters, Press 2 for digits, Press 3 for all characters: "))
-
-def password_generator(choice, length):
+def password_generator(length):
     letters= string.ascii_letters
     special_characters= string.punctuation
     numbers=string.digits
-    lettersNumbersCharacters= letters+numbers+special_characters #initialzing variable for all 3
+    CharacterSet=""
     password=""
-    while(len(password)<length):
+    choice=0 
+    while (choice!=4): #Loop until user needs to exit menu
+        print("What characters would you like to include in your password.\nMenu:\n1.Letters \n2.Numbers\n3.Special characters\n4.Exit Menu")
+        choice= int(input("Please enter your option:"))
         if (choice==1):
-            password+=random.choice(letters)
+            CharacterSet+= letters
         elif (choice==2):
-            password+=random.choice(numbers)
+            CharacterSet+= numbers
         elif (choice==3):
-            password+=random.choice(lettersNumbersCharacters)
+            CharacterSet+= special_characters
+        elif (choice==4):
+            break
         else:
-            choice= input("Enter a valid option")
-    print("Your password: "+password)
+            choice= input("Enter a valid option:")
+    while(len(password)<length):
+        password+=random.choice(CharacterSet)
+    print("Your password is: "+password)
 
-password_generator(choice,length)
+length= int(input("What is the length of the password?: "))
+password_generator(length) #calling the function
+        
 
+        
+    
 
 
 
